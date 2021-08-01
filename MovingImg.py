7e8f8a9e810930867cafa5824616194375a507bf
CodeWithKartikk <84595018+CodeWithKartikk@users.noreply.github.com>
2021-08-01 11:50:43 +0530

    Moving an Image in Pygame Window

---
+++ MovingImg.py
@@ -0,0 +1,27 @@
+# surface = pygame.surface(100,100)
+# surface = pygame.surface((100,100), pygame.SRCALPHA)
+
+import pygame
+pygame.init()
+white = (255,255,255)
+
+display_width = 800
+display_height = 600
+display_surface = pygame.display.set_mode((display_width, display_height))
+
+# set the pygame window name
+pygame.display.set_caption('Pygame Adding An Image')
+
+# Creating surface Object, image is drawn on it.
+image = pygame.image.load(r'C:\Users\Kathu\OneDrive\Desktop\Pygame\racecar.png')
+
+# infinite loop or game loop
+while True:
+    display_surface.fill(white)
+    display_surface.blit(image,(0,0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        pygame.display.update()
